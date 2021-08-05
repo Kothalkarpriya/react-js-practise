76470dac6f5f35179cbd5d6e911faae7bde2d491
Priya Kothalkar <54578811+Kothalkarpriya@users.noreply.github.com>
2021-08-05 19:10:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-var wel = "Welcome!";
-// var color = "blue";
-// var name = prompt("What's you name?");
+var emojiDictionary = {
+  "😁": "smiling broadly",
+  "😂": "Tears with joy",
+  "😊": "Smiling",
+  "😉": "Winking ",
+  "🤗": "Hugging Face",
+  "🤫": "Shushing Face",
+  "🤔": "Thinking Face",
+  "😒": "Unamused Face"
+};
 
-var heading = " Hello ALians!";
-// var Click = 0;
+var emojiWeKnow = Object.keys(emojiDictionary);
+
 export default function App() {
-  const [Click, watchClick] = useState(0);
-  function clickHandler() {
-    // console.log("Click", watchClick);
-    watchClick(Click + 1);
+  var [meaning, setMeaning] = useState("");
+  // var [userInput, setUserInput] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "we don't have this in outr database";
+    }
+    setMeaning(meaning);
   }
 
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
   return (
     <div className="App">
-      <h1>{heading}</h1>
-      <h2>{wel}</h2>
-      <button onClick={clickHandler}> Click me</button>
-      {Click}
+      <h1>Emoji Cyclopedia!</h1>
+      <input
+        style={{ padding: "2rem", backgroundColor: "#ecfccb", width: "70%" }}
+        onChange={emojiInputHandler}
+      ></input>
+      <h2 style={{ backgroundColor: "#86efac" }}>{meaning}</h2>
+
+      <h3> Emojis we know </h3>
+
+      {emojiWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
